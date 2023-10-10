d50710060c4b4b346cbdbaf885918b0f44e2a8e8
Edgar Hakobyan <ehakobya@asu.edu>
2023-10-09 23:49:26 -0700

    Task#44 - Add component selection section

--- src/Views/ARView/ARView.jsx
+++ src/Views/ARView/ARView.jsx
@@ -4,55 +4,53 @@ import { Interactive, useHitTest, useXR } from "@react-three/xr";
 import { useRef, useState } from "react";
 import Cube from "./Cube";
 
+const ARView = () => {
+  const reticleRef = useRef();
+  const [cubes, setCubes] = useState([]);
 
-const XrHitCube = () => {
+  const { isPresenting } = useXR();
 
-    const reticleRef = useRef();
-    const [cubes, setCubes] = useState([]);
-    const { isPresenting } = useXR();
-
-    useThree(({ camera }) => {
-        if (!isPresenting) {
-            camera.position.z = 3;
-        }
-    });
-
-    useHitTest((hitMatrix, hit) => {
-        hitMatrix.decompose(
-            reticleRef.current.position,
-            reticleRef.current.quaternion,
-            reticleRef.current.scale
-        );
-
-        reticleRef.current.rotation.set(-Math.PI / 2, 0, 0);
-    });
-
-    const placeCube = (e) => {
-        let position = e.intersection.object.position.clone();
-        let id = Date.now(); // unique id for each cube
-        setCubes([...cubes, { position, id }]);
+  useThree(({ camera }) => {
+    if (!isPresenting) {
+      camera.position.z = 3;
     }
+  });
 
-    return (
-        <>
-            <OrbitControls />
-            <ambientLight />
-            {isPresenting &&
-                cubes.map(({ position, id }) => {
-                    return <Cube key={id} position={position} />;
-                })}
-            {isPresenting && (
-                <Interactive onSelect={placeCube}>
-                    <mesh ref={reticleRef} rotation-x={-Math.PI / 2}>
-                        <ringGeometry args={[0.1, 0.25, 32]} />
-                        <meshStandardMaterial color={"white"} />
-                    </mesh>
-                </Interactive>
-            )}
-
-            {!isPresenting && <Cube />}
-        </>
+  useHitTest((hitMatrix, hit) => {
+    hitMatrix.decompose(
+      reticleRef.current.position,
+      reticleRef.current.quaternion,
+      reticleRef.current.scale
     );
+
+    reticleRef.current.rotation.set(-Math.PI / 2, 0, 0);
+  });
+
+  const placeCube = (e) => {
+    let position = e.intersection.object.position.clone();
+    let id = Date.now();
+    setCubes([...cubes, { position, id }]);
+  };
+
+  return (
+    <>
+      <OrbitControls />
+      <ambientLight />
+      {isPresenting &&
+        cubes.map(({ position, id }) => {
+          return <Cube key={id} position={position} />;
+        })}
+      {isPresenting && (
+        <Interactive onSelect={placeCube}>
+          <mesh ref={reticleRef} rotation-x={-Math.PI / 2}>
+            <ringGeometry args={[0.1, 0.25, 32]} />
+            <meshStandardMaterial color={"white"} />
+          </mesh>
+        </Interactive>
+      )}
+      {!isPresenting && <Cube />}
+    </>
+  );
 };
 
-export default XrHitCube;
+export default ARView;
