bc75938a202254b253fd6444323b5811b208c76e
Edgar Hakobyan <ehakobya@asu.edu>
2023-10-18 10:31:26 -0700

    Add "Help" page content and navigation button

--- src/Views/HelpView/HelpViewContainer.jsx
+++ src/Views/HelpView/HelpViewContainer.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
+import { BsFillArrowLeftCircleFill as BackArrow } from 'react-icons/bs';
+
+import './HelpView.css'
 
 const HelpViewContainer = () => {
-  return (
-    <div>Help</div>
-  )
+
+    const navigate = useNavigate();
+
+    return (
+        <div className='view-container'>
+            <div>
+                <button onClick={() => navigate('/')} className='back-button'><BackArrow /></button>
+            </div>
+            <div className='text-container'>
+                <h1 className='title'>HELP</h1>
+                <p className='description'>
+                    Sunt consequat nisi deserunt qui sint elit ullamco labore sit ex. Consectetur excepteur nulla ut exercitation nulla officia minim sit nostrud commodo velit Lorem Lorem. Irure dolore cillum ea commodo. Amet nisi consequat tempor deserunt minim sint nostrud nulla deserunt eiusmod. Ex fugiat pariatur veniam in aliquip voluptate laborum nulla. Id ut deserunt nulla nisi ullamco est elit consequat ullamco dolore ullamco et enim sunt.
+                </p>
+            </div>
+        </div>
+    )
 }
 
 export default HelpViewContainer
