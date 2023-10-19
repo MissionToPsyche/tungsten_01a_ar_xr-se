4499d4e4030e88db505dc222f467fd88d00e1e61
Edgar Hakobyan <ehakobya@asu.edu>
2023-10-18 20:27:39 -0700

    WIP: working on implementing back button logic
    Work in progress. Working on back button logic implementation to stop the AR session and redirect the user the the home screen

---
+++ src/Views/ARView/Interface.jsx
@@ -0,0 +1,47 @@
+import React, { forwardRef } from "react";
+// import { useCharacterAnimations } from "../../contexts/CharacterAnimations";
+
+const Interface = forwardRef((props, ref) => {
+    //   const { animations, animationIndex, setAnimationIndex } =
+    //     useCharacterAnimations();
+
+    const buttonArray = [
+        'Button 1',
+        'Button 2',
+        'Button 3',
+        'Button 4',
+        'Button 5',
+        'Button 6',
+        'Button 7',
+        'Button 8',
+        'Button 9',
+        'Button 10'
+    ];
+
+    return (
+        <div id="overlay-content" ref={ref}>
+            <div className="dom-container">
+                <div className="nav-container">
+                    <button onClick={() => console.log("DEBUG: Nav Button Clicked")}>BACK</button>
+                    <button onClick={() => console.log("DEBUG: Nav Button Clicked")}>INFO</button>
+                </div>
+                <div className="button-container">
+                    {/* {animations.map((animation, index) => (
+                        <button
+                            key={animation}
+                            className={`button ${index === animationIndex ? "active" : ""}`}
+                            onClick={() => setAnimationIndex(index)}
+                        >
+                            {animation}
+                        </button>
+                    ))} */}
+                    {buttonArray.map((button, index) => (
+                        <button onClick={() => console.log("DEBUG: Button Clicked")} className='select-button' key={index}>{button}</button>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+});
+
+export default Interface;
