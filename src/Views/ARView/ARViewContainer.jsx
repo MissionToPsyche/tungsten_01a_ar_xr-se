e5de308c39e09172696c93f122fe021d355e923f
Mia <mharmon7@asu.edu>
2023-10-25 19:53:34 -0400

    Add task#66 - intro popup logic

--- src/Views/ARView/ARViewContainer.jsx
+++ src/Views/ARView/ARViewContainer.jsx
@@ -4,11 +4,12 @@ import { useCallback, useState } from "react";
 
 import ARView from "./ARView";
 import Interface from "./Interface";
-
+import IntroPopup from "./IntroPopup";
 
 const ARViewContainer = () => {
 
     const [overlayContent, setOverlayContent] = useState(null);
+    const [showIntro, setShowIntro] = useState(true);
 
     let interfaceRef = useCallback((node) => {
         if (node !== null) {
@@ -16,24 +17,30 @@ const ARViewContainer = () => {
         }
     });
 
-    return (
+    const handleCloseIntro = () => {
+        setShowIntro(false);
+      }
+
+      return (
         <>
-            <ARButton
-                className="ar-button"
-                sessionInit={{
-                    requiredFeatures: ["hit-test"],
-                    optionalFeatures: ["dom-overlay"],
-                    domOverlay: { root: overlayContent },
-                }}
-            />
-            <Canvas>
-                <XR>
-                    <ARView />
-                </XR>
-            </Canvas>
-            <Interface ref={interfaceRef} />
+          {showIntro && <IntroPopup onClose={handleCloseIntro} />}
+          <ARButton
+            className="ar-button"
+            sessionInit={{
+              requiredFeatures: ["hit-test"],
+              optionalFeatures: ["dom-overlay"],
+              domOverlay: { root: overlayContent },
+            }}
+          />
+          <Canvas>
+            <XR>
+              <ARView />
+            </XR>
+          </Canvas>
+          <Interface ref={interfaceRef} />
         </>
-    );
-};
+      );
+    };
+    
 
 export default ARViewContainer;
