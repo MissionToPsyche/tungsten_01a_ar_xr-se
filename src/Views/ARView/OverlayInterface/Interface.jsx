314b4b19ac7896d61fc5a7148b97880c12f6d50d
Mia <mharmon7@asu.edu>
2023-11-03 22:19:27 -0400

    Add interface interactions

--- src/Views/ARView/OverlayInterface/Interface.jsx
+++ src/Views/ARView/OverlayInterface/Interface.jsx
@@ -1,57 +1,62 @@
-import React, { forwardRef, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { forwardRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useARContext } from '../Contexts/ARContext';
-import GameInfoPopup from "../GameInfoPopup/GameInfoPopup";
 import './Interface.css';
-import Cube from "../Cube";
-
+import LeftWingPopup from '../GameInfoPopup/LeftWingPopup';
+import RightWingPopup from '../GameInfoPopup/RightWingPopup';
+import GammaRayPopup from '../GameInfoPopup/GammaRayPopup';
+import NeutronSpectrometerPopup from '../GameInfoPopup/NeutronSpectrometerPopup';
+//import GameInfoPopup from '../GameInfoPopup/GameInfoPopup';
 import { FaHome, FaInfoCircle, FaBackward } from 'react-icons/fa';
 
 const Interface = forwardRef((props, ref) => {
-    //   const { animations, animationIndex, setAnimationIndex } =
-    //     useCharacterAnimations();
     const navigate = useNavigate();
-
-    const [showInfo, setShowInfo] = useState(false);
-
     const arViewRef = useARContext();
 
+    // Popup visibility states
+    const [showLeftWingPopup, setShowLeftWingPopup] = useState(false);
+    const [showRightWingPopup, setShowRightWingPopup] = useState(false);
+    const [showGammaRayPopup, setShowGammaRayPopup] = useState(false);
+    const [showNeutronSpectrometerPopup, setShowNeutronSpectrometerPopup] = useState(false);
+    const [showInfo, setShowInfo] = useState(false);
+
+    // Popup toggle functions
+    const togglePopup = (setPopupState) => () => setPopupState(prev => !prev);
+
+    // Handle back button click
     const handleBackButtonClick = () => {
-        // Call the function in ARView from Interface
-        if (arViewRef.current) {
-            arViewRef.current.invokeFunctionInARView();
-        }
+        arViewRef.current?.invokeFunctionInARView();
     };
 
+    // Info popup handlers
     const showInfoPopup = () => {
-        console.log("Info Popup Opened")
+        console.log('Info Popup Opened');
         setShowInfo(true);
     };
 
     const closeInfoPopup = () => {
-        console.log("Info Popup Closed")
-        setShowInfo(prevShowInfo => !prevShowInfo);
+        console.log('Info Popup Closed');
+        setShowInfo(prev => !prev);
     };
 
     return (
-        <div id="overlay-content" ref={ref}>
-            <div className="dom-container">
-                <div className="nav-container">
+        <div id='overlay-content' ref={ref}>
+            <div className='dom-container'>
+                <div className='nav-container'>
                     <button onClick={handleBackButtonClick}>BACK</button>
                     <button onClick={showInfoPopup}>INFO</button>
                 </div>
-                <div className="button-container">
-                    <button className='select-button'><FaHome /></button>
-                    <button className='select-button'><FaInfoCircle /></button>
-                    <button className='select-button'><FaBackward /></button>
-                    <button className='select-button'><FaHome /></button>
-                    <button className='select-button'><FaInfoCircle /></button>
-                    <button className='select-button'><FaBackward /></button>
-                    <button className='select-button'><FaHome /></button>
-                    <button className='select-button'><FaInfoCircle /></button>
-                    <button className='select-button'><FaBackward /></button>
+                <div className='button-container'>
+                    <button className='select-button' onClick={togglePopup(setShowLeftWingPopup)}><FaHome /></button>
+                    <button className='select-button' onClick={togglePopup(setShowRightWingPopup)}><FaInfoCircle /></button>
+                    <button className='select-button' onClick={togglePopup(setShowGammaRayPopup)}><FaBackward /></button>
+                    <button className='select-button' onClick={togglePopup(setShowNeutronSpectrometerPopup)}><FaHome /></button>
                 </div>
             </div>
+            {showLeftWingPopup && <LeftWingPopup onClose={togglePopup(setShowLeftWingPopup)} />}
+            {showRightWingPopup && <RightWingPopup onClose={togglePopup(setShowRightWingPopup)} />}
+            {showGammaRayPopup && <GammaRayPopup onClose={togglePopup(setShowGammaRayPopup)} />}
+            {showNeutronSpectrometerPopup && <NeutronSpectrometerPopup onClose={togglePopup(setShowNeutronSpectrometerPopup)} />}
             {showInfo && <GameInfoPopup onClose={closeInfoPopup} />}
         </div>
     );
