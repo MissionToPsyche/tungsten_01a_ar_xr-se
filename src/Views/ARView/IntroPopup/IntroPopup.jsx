779b77df8d98a7deeefbca351c41705291313f4b
Mia <mharmon7@asu.edu>
2023-11-06 18:08:39 -0500

    Add meteor to intro popup

--- src/Views/ARView/IntroPopup/IntroPopup.jsx
+++ src/Views/ARView/IntroPopup/IntroPopup.jsx
@@ -5,6 +5,7 @@ const IntroPopup = ({ onClose }) => {
   return (
     <div className="popup-overlay">
       <div className="popup-container">
+        <img src="/assets/meteor.jpg" alt="Meteor"/>
         <h2>Welcome to the AR Game!</h2>
         <p>Instructions on how to play the game go here...</p>
         <button onClick={onClose}>Close</button>
