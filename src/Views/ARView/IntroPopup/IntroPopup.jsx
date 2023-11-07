f7d6774f341a318c2e6e6bc4beb35b82a934aeb0
Mia <mharmon7@asu.edu>
2023-11-06 20:25:54 -0500

    Add temp intro.css

--- src/Views/ARView/IntroPopup/IntroPopup.jsx
+++ src/Views/ARView/IntroPopup/IntroPopup.jsx
@@ -3,12 +3,14 @@ import "./IntroPopup.css";
 
 const IntroPopup = ({ onClose }) => {
   return (
-    <div className="popup-overlay">
-      <div className="popup-container">
-        <img src="/assets/meteor.jpg" alt="Meteor"/>
-        <h2>Welcome to the AR Game!</h2>
-        <p>Instructions on how to play the game go here...</p>
-        <button onClick={onClose}>Close</button>
+    <div className="popup-container">
+      <div className="popup-background">
+        <img className="popup-image" src="/assets/meteor.jpg" alt="Meteor"/> 
+        <div className="popup-content">
+          <h2>Welcome to the AR Game!</h2>
+          <p>Instructions on how to play the game go here...</p>
+        </div>
+        <button className="popup-button" onClick={onClose}>CLOSE</button>
       </div>
     </div>
   );
