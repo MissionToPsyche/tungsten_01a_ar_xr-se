0932689c2d172bd415d708e0576140593fcd9a59
Edgar Hakobyan <ehakobya@asu.edu>
2023-11-13 20:42:05 -0800

    Add common constants file and refactor assets

---
+++ src/Context/CommonConstants.jsx
@@ -0,0 +1,21 @@
+const SpacecraftIcons = {
+    ANTENNA: '/icons/antenna.png',
+    BUS: '/icons/bus_with_structure.png',
+    GAMMA_RAY: '/icons/gamma_ray.png',
+    LEFT_WING: '/icons/left_wing.png',
+    NEUTRON_SPECTROMETER: '/icons/neutron_spectrometer.png',
+    RIGHT_WING: '/icons/right_wing.png',
+  };
+  
+  const SpacecraftComponents = {
+    ANTENNA: '/models/gltf-files/antenna.gltf',
+    BUS: '/models/gltf-files/bus.gltf',
+    BUS_WITH_STRUCTURE: '/models/gltf-files/bus_with_structure.gltf',
+    GAMMA_RAY: '/models/gltf-files/gamma_ray.gltf',
+    LEFT_WING: '/models/gltf-files/left_wing.gltf',
+    NEUTRON_SPECTROMETER: '/models/gltf-files/neutron_spectrometer.gltf',
+    RIGHT_WING: '/models/gltf-files/right_wing.gltf',
+  };
+  
+  export { SpacecraftIcons, SpacecraftComponents };
+  
