3803464b082f7f3b24fe8870854c3ccdc62f8117
senicola <98562478+senicola@users.noreply.github.com>
2024-01-19 02:47:40 -0700

    Work in Progress: Task 142
    Trying to see if it's possible to call function from pop up for each piece

--- src/Views/ARView/GameInfoPopup/LeftWingPopup.jsx
+++ src/Views/ARView/GameInfoPopup/LeftWingPopup.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import './SatellitePiecesPopup.css';
 import { SpacecraftIcons } from '../../../Context/CommonConstants';
+import SpacecraftAssembly from '../../../Models/SpacecraftAssembly';
+//import {undoGreyedOutEffect, SpacecraftAssembly} from '../../../Models/SpacecraftAssembly';
+//import {SpacecraftAssembly}
+
+
 
 const LeftWingPopup = ({ onClose }) => {
+
+    const handleClick = () => {
+        let wingLGLTF;
+        const onReady = (greyRef) => {
+            wingLGLTF = greyRef;
+          };
+          return <SpacecraftAssembly position={[-1.37, -0.03, -0.015]} onReady={onReady} />;
+        onClose();
+      };
     return (
         <div className="popup-container">
             <div className="popup-content">
@@ -17,7 +31,7 @@ const LeftWingPopup = ({ onClose }) => {
                 <div className="popup-text">
                     The spacecraft is equipped with large solar panel arrays that extend from the body of the bus. These solar arrays harness solar energy to power the spacecraft's systems.
                 </div>
-                <button className="popup-button" onClick={onClose}>
+                <button className="popup-button" onClick={handleClick}>
                     CONTINUE
                 </button>
             </div>
