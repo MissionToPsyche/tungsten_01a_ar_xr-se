a461796a03b35f062a8b681eb884a1ba8097937e
senicola <98562478+senicola@users.noreply.github.com>
2024-03-15 00:19:05 -0700

    Task#246 WIP- Added comments to AudioContext.jsx

--- src/Context/AudioContext.jsx
+++ src/Context/AudioContext.jsx
@@ -3,13 +3,13 @@ import React, { createContext, useState, useEffect } from 'react';
 export const AudioContext = createContext();
 
 export const AudioProvider = ({ children }) => {
-    const [isMusicPlaying, setIsMusicPlaying] = useState(false);
+    const [isMusicPlaying, setIsMusicPlaying] = useState(false);//music is not turned on until user turns it on in settings
     const [audio] = useState(new Audio('../public/audio/space_music.mp3'));
-    const [soundEffectsEnabled, setSoundEffectsEnabled] = useState(true); 
+    const [soundEffectsEnabled, setSoundEffectsEnabled] = useState(true); //Sounds effects enabled at start, can be turned off in settings
 
     useEffect(() => {
-        audio.loop = true;
-        isMusicPlaying ? audio.play() : audio.pause();
+        audio.loop = true;//Audio file loops for continuous music 
+        isMusicPlaying ? audio.play() : audio.pause();//If the music is playing it pauses, if not the music starts
     }, [isMusicPlaying, audio]);
 
     return (
