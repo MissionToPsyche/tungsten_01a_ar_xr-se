734ce8127901ebdf63ed814d95362c0431c6f2c3
senicola <98562478+senicola@users.noreply.github.com>
2024-03-19 17:31:45 -0700

    Task#247 WIP- Added comments to GammaRayPopup.jsx

--- src/Views/ARView/GameInfoPopup/GammaRayPopup.jsx
+++ src/Views/ARView/GameInfoPopup/GammaRayPopup.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './SatellitePiecesPopup.css';
 import { SpacecraftIcons } from '../../../Context/CommonConstants';
-
+//Game Tile pop-up that states information on the gamma ray 
+//Includes a title, image of the instrument, description, and a button to close
 const GammaRayPopup = ({ onClose }) => {
     return (
         <div className="popup-container">
