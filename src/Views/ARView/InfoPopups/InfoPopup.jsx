95a0d78ca1333f0d7ed0d2ae2d505b5ab96f3203
senicola <98562478+senicola@users.noreply.github.com>
2024-03-20 17:16:02 -0700

    Task#248 WIP- Added Comments to InfoPopup.jsx

--- src/Views/ARView/InfoPopups/InfoPopup.jsx
+++ src/Views/ARView/InfoPopups/InfoPopup.jsx
@@ -3,6 +3,9 @@ import "../GameInfoPopup/SatellitePiecesPopup.css";
 import { popupText } from "./InfoPopupContent";
 ("./InfoPopupContent");
 import HelpViewContainer from "../../HelpView/HelpViewContainer";
+
+//Info pop-up on the AR View that is opened if the user presses the "i" in the top right corner. Explains to user how to play the game. 
+//Includes a title, explanation text, and a button to close
 const InfoPopup = ({ onClose }) => {
   return (
     <div className="popup-container">
